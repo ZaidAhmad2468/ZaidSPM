c7212e13fad3c622d03c958ee5abf68cdab407ec
ZaidAhmad2468 <45063881+ZaidAhmad2468@users.noreply.github.com>
2022-04-05 21:50:20 +0500

    fg

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "ZaidSPM",
+    name: "Onfido",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "ZaidSPM",
-            targets: ["ZaidSPM"]),
+            name: "Onfido",
+            targets: ["Onfido"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,12 +18,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "ZaidSPM",
-            dependencies: []),
-        .testTarget(
-            name: "ZaidSPMTests",
-            dependencies: ["ZaidSPM"]),
+     
         .binaryTarget(
                    name: "Onfido",
                    url: "https://s3-eu-west-1.amazonaws.com/onfido-sdks/ios/Onfido-v24.4.0.zip",
