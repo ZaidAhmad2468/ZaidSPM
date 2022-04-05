cb7cc64c3e47bd5fb3462da72f43cb8750a65b52
ZaidAhmad2468 <45063881+ZaidAhmad2468@users.noreply.github.com>
2022-04-05 20:09:41 +0500

    first

--- Package.swift
+++ Package.swift
@@ -24,5 +24,10 @@ let package = Package(
         .testTarget(
             name: "ZaidSPMTests",
             dependencies: ["ZaidSPM"]),
+        .binaryTarget(
+                   name: "Onfido",
+                   url: "https://s3-eu-west-1.amazonaws.com/onfido-sdks/ios/Onfido-v24.4.0.zip",
+                   checksum: "0e111f4ea516986496f6724b28246f7a858210eea79295755ab1cb5c640c6990"
+               )
     ]
 )
